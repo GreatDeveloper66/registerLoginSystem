af760f78502a4c231255761dfb1aa77767fb19f1
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-20 16:18:41 -0500

    Add express static lines to server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express()
 const PORT = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
 app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static(path.join(path.resolve(), "client","build")))
+
 app.get('*', (req,res) => {
     res.sendFile(path.join(path.resolve(), "client", "build", "index.html"))
 })
