312088824340345e6e21d527895a25bce5b6cdfe
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-20 16:12:47 -0500

    Add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import bodyPaser from 'body-parser'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 import path from 'path'
 const app = express()
 const PORT = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 app.get('*', (req,res) => {
     res.sendFile(path.join(path.resolve(), "client", "build", "index.html"))
 })
