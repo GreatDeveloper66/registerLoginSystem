13794d5876bbf9756457f87f925ad3ba57bda7cc
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-20 17:37:31 -0500

    Add login button to login screen

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
 
 function Login() {
-    return(<div>Login</div>)
+    return(
+    <div>
+        <h1>Login</h1>
+        <Button>LOGIN</Button>
+    </div>
+    )
 }
 
 export default Login
