694dc877e7cf55f4b6c748fb397f132fc69044ce
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-20 14:39:22 -0500

    Add server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+app.get('*', (req,res) => {
+    res.sendFile(path.join(path.resolve(), "client", "build", "index.html"))
+})
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}.`);
+});
