d3a106fbb0c6d513234f8c79cc3960944e0df466
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-20 16:06:32 -0500

    Add body parser statements

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import path from 'path'
 const app = express()
 const PORT = 3000
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get('*', (req,res) => {
     res.sendFile(path.join(path.resolve(), "client", "build", "index.html"))
 })
