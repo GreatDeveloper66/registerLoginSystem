aeaae3c2769e5188826a2d63a5beb605587e18df
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-20 17:27:58 -0500

    Add component imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './Components/Login'
+import Register from './Components/Register'
+import Protected from './Components/Protected'
 
 
 function App() {
