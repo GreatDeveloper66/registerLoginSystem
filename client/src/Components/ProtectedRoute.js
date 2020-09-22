acd58eccd003b33273da8406b0eb74fbd2e3be44
GreatDeveloper66 <ashaffe@gmail.com>
2020-09-21 19:01:40 -0500

    Add protected route

---
+++ client/src/Components/ProtectedRoute.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Auth from './Auth'
+import { Route } from 'react-router-dom'
+
+
+export const ProtectedRoute = ({ component: Component, ...rest }) => {
+    return (
+        <Route {...rest} render={props => {
+            if(Auth.isAuthenticated()){
+                return <Component {...props} 
+                />
+            }
+            else {
+                return <Redirect to={{
+                    pathname: "/",
+                    state: {
+                        from: props.location
+                    }
+                }}
+                />
+            }
+             }} />
+    )
+}
+
+
